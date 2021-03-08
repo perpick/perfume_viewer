506b8d7472d36a6645494f648cd6611eddd3cebf
rayleighko <ray@scentlab.kr>
2021-03-08 18:28:44 +0900

    fix: styles

--- src/Perfumes/PerfumeCard.tsx
+++ src/Perfumes/PerfumeCard.tsx
@@ -7,26 +7,29 @@ import useStyles from './useStyles'
 
 const PerfumeCard: React.FC<{ perfume: Perfume }> = ({ perfume }) => {
     const classes = useStyles();
+
+    const handleClickImage = () => { window.open(perfume.img_url, "_blank") }
+
     return (
         <Card className={ classes.root }>
-            <CardActionArea onClick={ () => { window.open(perfume.img_url, "_blank") } }>
+            <CardActionArea onClick={ handleClickImage }>
                 <CardMedia
                     className={ classes.media }
                     image={ perfume.img_url }
                     title={ perfume.title }
                 />
-                <CardContent>
-                    <Typography gutterBottom variant="h4" component="h2">
-                        { perfume.source }
-                    </Typography>
-                    <Typography gutterBottom variant="h6" component="h2">
-                        { perfume.title }
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        { perfume._id }
-                    </Typography>
-                </CardContent>
             </CardActionArea>
+            <CardContent>
+                <Typography gutterBottom variant="h4" component="h2">
+                    { perfume.source }
+                </Typography>
+                <Typography gutterBottom variant="h6" component="h2">
+                    { perfume.title }
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    { perfume._id }
+                </Typography>
+            </CardContent>
         </Card>)
 }
 
