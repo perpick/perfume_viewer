913a45dda6efb4554d78d0bbbed17c696725fd6a
rayleighko <ray@scentlab.kr>
2021-03-08 14:10:21 +0900

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,29 +1,50 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
+  },
+  alias: {
+    "@src": "./src",
   },
   plugins: [
-    '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+    [
+      "@snowpack/plugin-optimize",
+      {
+        preloadModules: true,
+      },
+    ],
+    [
+      "@snowpack/plugin-build-script",
+      {
+        cmd: "postcss",
+        input: [".css"],
+        output: [".css"],
+      },
+    ],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
   packageOptions: {
-    /* ... */
+    installTypes: true,
+    polyfillNode: true,
   },
   devOptions: {
-    /* ... */
+    port: 26826,
   },
   buildOptions: {
-    /* ... */
+    clean: true,
+    out: "dist",
+  },
+  optimize: {
+    bundle: true,
+    minify: true,
+    treeshake: true,
+    target: "es2020",
   },
 };
